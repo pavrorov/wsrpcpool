ea9234015719edf4da58770bc2265679e75df963
Pavel Avrorov <avrorov@priboy.online>
2017-03-12 16:10:23 +0300

    Implement the pool map

--- wsrpcpool.go
+++ wsrpcpool.go
@@ -4,15 +4,19 @@ package wsrpcpool
 import (
 	"crypto/tls"
 	"crypto/x509"
-	//"golang.org/x/net/websocket"
+	"golang.org/x/net/websocket"
 	"io/ioutil"
 	"net/http"
-	//"net/rpc/jsonrpc"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+	"sync"
 )
 
 /* A Web-socket connection endpoint for RPC provideres. */
 type PoolServer struct {
 	http.Server
+	PoolMap     map[string]chan *rpc.Call
+	DefaultPool chan *rpc.Call
 }
 
 /* Plain PoolServer instance. */
@@ -83,4 +87,58 @@ func NewPoolTLSAuth(certfile, keyfile string, clientCAs ...string) (*PoolServer,
 	}
 
 	return pool, err
+}
+
+/* Passes the given call to the client. */
+func invoke(client *rpc.Client, call *rpc.Call) *rpc.Call {
+	return client.Go(call.ServiceMethod, call.Args, call.Reply, call.Done)
+}
+
+/* Associates the given path with the set of remote objects or make
+it the default path if no object names given. */
+func (pool *PoolServer) BindPath(path string, names ...string) {
+	if pool.Handler == nil {
+		pool.Handler = http.NewServeMux()
+	}
+
+	mux := pool.Handler.(*http.ServeMux)
+
+	if len(names) > 0 {
+		if pool.PoolMap == nil {
+			pool.PoolMap = make(map[string]chan *rpc.Call)
+		}
+		for _, name := range names {
+			if pool.PoolMap[name] == nil {
+				pool.PoolMap[name] = make(chan *rpc.Call)
+			}
+		}
+		mux.Handle(path, websocket.Handler(func(ws *websocket.Conn) {
+			var wg sync.WaitGroup
+			client := jsonrpc.NewClient(ws)
+			for _, name := range names {
+				callIn := pool.PoolMap[name]
+				go func() {
+					defer wg.Done()
+					for c := range callIn {
+						invoke(client, c)
+					}
+				}()
+			}
+			wg.Add(len(names))
+			wg.Wait()
+			client.Close()
+		}))
+	} else {
+		if pool.DefaultPool == nil {
+			pool.DefaultPool = make(chan *rpc.Call)
+		}
+		chanIn := pool.DefaultPool
+		mux.Handle(path, websocket.Handler(func(ws *websocket.Conn) {
+			client := jsonrpc.NewClient(ws)
+			for c := range chanIn {
+				invoke(client, c)
+			}
+			client.Close()
+		}))
+	}
 }
